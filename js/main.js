ba65519d59daa6847a00f5b2a66dc7a0829dc7d9
Ivan gorshinev <IvanQAEngineer@yandex.ru>
2023-10-08 23:20:30 +0300

    burger

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const burgerBtn = document.getElementById('burger')
+const head = document.querySelector('.header')
+
+document.addEventListener('DOMContentLoaded', function () {
+  burgerBtn.onclick = () => {
+    if (head.classList.toggle('open')) {
+     
+    }
+   
+  }
+})
